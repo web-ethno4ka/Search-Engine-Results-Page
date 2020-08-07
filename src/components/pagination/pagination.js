3eb6d6c26d87cde9f40838f5a9bbb1562a9b3fb6
valeriy mixailov <ethno4ka@gmail.com>
2020-08-08 01:32:11 +0300

    Adding styles for components

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './pagination.scss';
 
 export default function Pagination() {
   return (
--- src/components/product-card/rate.js
+++ src/components/product-card/rate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './rate.scss';
 
 export default function Rate() {
   return (
